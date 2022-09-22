087a3bd761f7daf77ce51a7877a087cc0cb44391
darrenlee <LEEYH2@ETSU.EDU>
2022-09-22 14:25:09 -0400

    Lab 5

---
+++ lab5/FileRoot.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace lab5
+{
+    public class FileRoot
+    {
+        public static string projectRoot = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+    }
+}
+
